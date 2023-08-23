e980aa2aba30a6ae46e64bcd2b1172ccdc3cafa3
Koscee <52253099+Koscee@users.noreply.github.com>
2023-08-23 19:14:39 +0800

    create a Paddle that responds to key press events

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from turtle import Screen
+from turtle import Turtle
 
 screen = Screen()
 screen.title("Pong")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
+screen.tracer(0)  # turns off animation on the screen
 
+paddle = Turtle()
+paddle.color("white")
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()  # refresh screen
 
 screen.exitonclick()
