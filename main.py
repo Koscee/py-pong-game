d217159077d99e16c1b3aaf8e83eaf9a1546b02b
Koscee <52253099+Koscee@users.noreply.github.com>
2023-08-23 20:24:39 +0800

    create ball and make it move

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.title("Pong")
@@ -9,6 +11,7 @@ screen.tracer(0)  # turns off animation on the screen
 
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
@@ -18,6 +21,8 @@ screen.onkey(left_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()  # refresh screen
+    ball.move()
 
 screen.exitonclick()
